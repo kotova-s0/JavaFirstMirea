98b6efa1cf56615211cfbd8c9f287a2058b617eb
Sofia <kotovasofia1102@yandex.ru>
2021-10-13 23:42:45 +0300

    коммит 4 получаеца

---
+++ src/ru/mirea/task10/Fifth.java
@@ -0,0 +1,15 @@
+package ru.mirea.task10;
+
+public class Fifth {
+    public static int recursion(int n) {
+        if (n < 10) {
+            return n;
+        }
+        else {
+            return n % 10 + recursion(n / 10);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(recursion(123));
+    }
+}
---
+++ src/ru/mirea/task10/Third.java
@@ -0,0 +1,19 @@
+package ru.mirea.task10;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Third {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a > b)
+            for (int i = a; i <= b; i++)
+                System.out.print(i + " ");
+        else
+            for (int i = a; i >= b; i--)
+                System.out.print(i + " ");
+    }
+}
